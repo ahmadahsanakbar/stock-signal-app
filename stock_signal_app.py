110a0aa66e14de98185cfb85726718354cff14b2
Ahmad Ahsan Akbar <158441117+ahmadahsanakbar@users.noreply.github.com>
2025-04-17 17:43:40 +0500

    Update stock_signal_app.py

--- stock_signal_app.py
+++ stock_signal_app.py
@@ -94,10 +94,12 @@ if uploaded_file is not None:
 # Footer with branding
 st.markdown("""---""")
 st.markdown(
-    "<div style='text-align: center;'>"
-    "📬 Developed by <b>Ahmad Ahsan Akbar</b>
+    """
+    <div style='text-align: center;'>
+    📬 Developed by <b>Ahmad Ahsan Akbar</b>
     🌐 <a href='https://ahmad-ahsan-akbar.me' target='_blank'>ahmad-ahsan-akbar.me</a> &nbsp; | &nbsp;
         📘 <a href='https://www.facebook.com/ahmadahsanakbar' target='_blank'>Facebook</a>"
-    "</div>",
+    </div>
+    """,
     unsafe_allow_html=True
 )
